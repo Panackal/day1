5a8c6af052e736a34306753c171486cbf9c5f90a
Panackal <134275873+Panackal@users.noreply.github.com>
2023-05-22 15:10:26 +0530

    1st commit

---
+++ joshua/script.js
@@ -0,0 +1,48 @@
+console.log("hello");
+console.log("thanks");
+let a=10;
+let b=4;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+let per={name:"JSON",place:"pbvr"};
+console.log(`${per.name} lives in ${per.place}`);
+let f=["j","o","s"];
+console.log(f);
+f.push("h")
+console.log(f);
+f.pop();
+f.unshift("xxx");
+console.log(f[0]);
+let cls=[{
+    name:"Jo",
+    age:20,
+    place:"koko"
+},
+{
+    name:"jojjoj",
+    age:55,
+    place:"wwww"
+}]
+console.log(cls[1].name);
+function sub(a,b){console.log(a-b)};
+sub(98,77);
+const mul =(a,b)=>{console.log(a*b)};
+mul(54,54);
+let q=5;
+let r=33;
+if (r<q) { console.log("truu");
+    
+} else {
+console.log("false");   
+}
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+let m=0;
+while (m<100) {
+    
+    console.log(m);
+    
+}
